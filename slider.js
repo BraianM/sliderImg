4d8599172d4ebde0de0fe33edb8a841977d6fb54
Braian Martinez <martinezbraian430@gmail.com>
2021-09-14 17:26:55 -0300

    Mi primera animación automatica

--- slider.js
+++ slider.js
@@ -1,7 +1,9 @@
-const listImg = []
+
+const listImg = [];
 
 window.onload = () => {
-	animation(element);
+	run();
+	//animation(listImg);
 }
 
 // metodo run
@@ -12,7 +14,14 @@ window.onload = () => {
 // generar boton right
 // mostrat indicor de cada imagen que va pasando en forma de puntos.
 
-const animation = (e) => {
+let run = () => {
+	addElements(listImg)
+	updateArrDom();
+	setInterval(()=>animation(listImg),8000);
+}
+
+const animation = (arr) => {
+	let elemRotar
 	let pos = 0;
 	let id = null;
 	id = setInterval(() => {
@@ -20,31 +29,46 @@ const animation = (e) => {
 			// limpiar intervalo
 			clearInterval(id);
 			// mecanismo de rotación de elementos.
+			elemRotar = arr.shift()
+			//console.log(elemRotar)
+			arr.push(elemRotar);
+			document.getElementById('pick-1').style.position = 'static';
+			document.getElementById('pick-2').style.position = 'static';
+			document.getElementById('pick-3').style.position = 'static';
+			updateArrDom()
 		} else {
 			pos++
-			e(1).style.right = pos + 'px';
-			e(3).style.right = pos + 'px';
-			e(5).style.right = pos + 'px';
+			document.getElementById('pick-1').style.right = pos + 'px';
+			document.getElementById('pick-2').style.right = pos + 'px';
+			document.getElementById('pick-3').style.right = pos + 'px';
 		}
 	}, 1);
 }
 
-
+let addElements = (arr) => {
+	let sum = 1;
+	for (let a = 0; a < document.getElementById('content-slider').childElementCount; a++) {
+		arr.push(document.getElementById('content-slider').childNodes[sum])
+		sum+=2;
+	}
+}
 
 // obtenemos la referencia de los elemento Img 
-const element = (index) => idContentSlider().childNodes[index];
-
-// obtenemos la referencia de content-slider por su id
-const idContentSlider = () => document.getElementById('content-slider'); 
-
+//const element = (index) => document.getElementById('content-slider').childNodes[index];
 
 // actualizamos el div content-slider para reflejar cambios en nuestro arreglo.
 const updateArrDom = () => {
-	let lista = '';
-	for (let i = 0; i < listImg.length ; i++) {
+	let lista = ''; 
+	for (let i = 0; i < listImg.length; i++) {
 		lista += listImg[i].outerHTML;
 	}
-	idContentSlider().innerHTML = lista;
+	document.getElementById('content-slider').innerHTML = lista;
+	document.getElementById('pick-1').style.right = '0px';
+	document.getElementById('pick-2').style.right = '0px';
+	document.getElementById('pick-3').style.right = '0px';
+	document.getElementById('pick-1').style.position = 'relative';
+	document.getElementById('pick-2').style.position = 'relative';
+	document.getElementById('pick-3').style.position = 'relative';
 }
 
 
