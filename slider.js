a204ae7b6c2615ef851aa0fd2a1f0a7fbfa4c8d0
Braian Martinez <martinezbraian430@gmail.com>
2021-09-22 01:58:51 -0300

    se agregaron los botones y se implemento su codificacion

--- slider.js
+++ slider.js
@@ -1,49 +1,59 @@
-const listIdImg = []
+const btLeft = document.getElementById('btLeft')
+const btRight = document.getElementById('btRight')
+const idContentSlider = document.getElementById('content-slider')
+const listElements = document.querySelectorAll('#content-slider img')
+
+const moveElement = (listElements, pos) => {
+	listElements.forEach((element) => {
+		element.style.position = 'relative';
+		element.style.right = pos + 'px';
+	})
+}
+
+const haciaAdelante = (idContentSlider, listElements) => {
+	listElements.forEach((element) => {
+		element.style.position = 'static';
+	})
+	idContentSlider.appendChild(idContentSlider.firstElementChild)
+}
+
+const haciaAtras = (idContentSlider) => {
+	idContentSlider.insertBefore(idContentSlider.lastElementChild, idContentSlider.firstElementChild)
+}
 
 window.onload = () => {
-	addElementArr();
-	removeImg();
-	updateSlider();
-	//printConsole();
+	btLeft.addEventListener('click', () => {
+		haciaAtras(idContentSlider)
+		btLeft.disabled = true;
+		btRight.disabled = true;
+		let a = 800;
+		id = setInterval(() => {
+			a--;
+			if (a === 0) {
+				clearInterval(id);
+				btLeft.disabled = false;
+				btRight.disabled = false;
+			} else {
+				moveElement(listElements, a);
+			}
+		}, 1, a)
+	})
+	
+	btRight.addEventListener('click', () => {
+		btRight.disabled = true;
+		btLeft.disabled = true;
+		let a = 0;
+		id = setInterval(() => {
+			a++;
+			if (a == 800) {
+				haciaAdelante(idContentSlider, listElements)
+				clearInterval(id);
+				btRight.disabled = false;
+				btLeft.disabled = false;
+			} else {
+				moveElement(listElements, a);
+			}
+		}, 1, a)
+	}, true)
 }
 
-// metodo run
-// identificar elementos
-// Agregar los elementos a un arreglo
-// asignar animacion a las imagenes
-// generar boton left
-// generar boton right
-// mostrat indicor de cada imagen que va pasando en forma de puntos.
-
-
-const addElementArr = () => {
-	let v = 1;
-	for(let i = 0; i < idContentSlider().childElementCount; i++) {
-		listIdImg.push(idImg(v));
-		v+=2;
-		//console.log(listIdImg[i]);
-	}
-}
-
-// obtenemos la referencia de los elemento Img por su id.
-const idImg = (index) => (idContentSlider().childNodes[index]);
-
-// obtenemos la referencia de content-slider por su id
-const idContentSlider = () => (document.getElementById('content-slider')); 
-
-
-// actualizamos el div content-slider para reflejar cambios en nuestro arreglo.
-const updateSlider = () => {
-	let lista = '';
-	for (let i = 0; i < listIdImg.length ; i++) {
-		lista += listIdImg[i].outerHTML;
-	}
-	console.log(lista);
-	idContentSlider().innerHTML = lista;
-}
-
-const removeImg = () => (listIdImg.pop());
-//--------------------------------------------------------------------------------------
-
-const printConsole = () => {
-}
